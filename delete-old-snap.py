1012181e80aaa7b73106f08b1eeabf8b77e20f00
Gustavo Duffrayer <guduffrayer@hotmail.com>
2019-10-09 15:50:36 -0300

    Update delete-old-snap.py

--- delete-old-snap.py
+++ delete-old-snap.py
@@ -3,59 +3,62 @@
 import boto3
 from botocore.exceptions import ClientError
 
-from datetime import datetime,timedelta
+import datetime 
+#from import datetime, timedelta, date
 
 def delete_snapshot(snapshot_id, reg):
-    print "Deleting snapshot %s " % (snapshot_id)
-    try:  
+    print ("Deleting snapshot %s ") % (snapshot_id)
+    try:
         ec2resource = boto3.resource('ec2', region_name=reg)
         snapshot = ec2resource.Snapshot(snapshot_id)
         snapshot.delete()
     except ClientError as e:
-        print "Caught exception: %s" % e
-        
-    return
-    
-def lambda_handler(event, context):
-    
-    # Get current timestamp in UTC
-    now = datetime.now()
+        print ("Caught exception: %s") % e
 
-	# AWS Account ID    
-	account_id = '1234567890'
-    
+    return
+
+def lambda_handler(event, context):
+
+    # Get current timestamp in UTC
+    #now = datetime.date.today()
+    now = datetime.datetime.now()
+    print (now)
+    #oldest_datetime = datetime.date.today() - datetime.timedelta(days=event['retention_days'])
+
+	# AWS Account ID
+    account_id = "xxxxxxxx"
+
     # Define retention period in days
-    retention_days = 10
-    
+    retention_days = 30
+
     # Create EC2 client
     ec2 = boto3.client('ec2')
-    
+
     # Get list of regions
     regions = ec2.describe_regions().get('Regions',[] )
 
     # Iterate over regions
     for region in regions:
-        print "Checking region %s " % region['RegionName']
+        print ("Checking region %s " % region['RegionName'])
         reg=region['RegionName']
-        
+
         # Connect to region
         ec2 = boto3.client('ec2', region_name=reg)
-        
+
         # Filtering by snapshot timestamp comparison is not supported
         # So we grab all snapshot id's
         result = ec2.describe_snapshots( OwnerIds=[account_id] )
-    
+
         for snapshot in result['Snapshots']:
-            print "Checking snapshot %s which was created on %s" % (snapshot['SnapshotId'],snapshot['StartTime'])
-       
+            print ("Checking snapshot %s which was created on %s") % (snapshot['SnapshotId'],snapshot['StartTime'])
+
             # Remove timezone info from snapshot in order for comparison to work below
             snapshot_time = snapshot['StartTime'].replace(tzinfo=None)
-        
-            # Subtract snapshot time from now returns a timedelta 
+            
+            # Subtract snapshot time from now returns a timedelta
             # Check if the timedelta is greater than retention days
-            if (now - snapshot_time) > timedelta(retention_days):
-                print "Snapshot is older than configured retention of %d days" % (retention_days)
-                delete_snapshot(snapshot['SnapshotId'], reg)
+            if (now - snapshot_time) > datetime.timedelta(retention_days):
+                print ("Snapshot is older than configured retention of %d days") % (retention_days)
+                #delete_snapshot(snapshot['SnapshotId'], reg)
             else:
-                print "Snapshot is newer than configured retention of %d days so we keep it" % (retention_days)          
-  
+                print ("Snapshot is newer than configured retention of %d days so we keep it") % (retention_days)
